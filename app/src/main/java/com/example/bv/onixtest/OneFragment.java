43a180a35dcc118a83e8d3d0f1fba8ea9e51d903
vadimbondar <undefinedm12@gmail.com>
2016-07-03 01:58:28 +0300

    Working on adding tabs

---
+++ app/src/main/java/com/example/bv/onixtest/OneFragment.java
@@ -0,0 +1,28 @@
+package com.example.bv.onixtest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bv on 03.07.16.
+ */
+public class OneFragment extends Fragment {
+    public OneFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one,container,false);
+    }
+}
