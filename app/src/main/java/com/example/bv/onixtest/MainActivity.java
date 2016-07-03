b0f4479e19dcd7d9f308a8c3f62edf1c01722694
vadimbondar <undefinedm12@gmail.com>
2016-07-03 18:44:48 +0300

    working on login button

--- app/src/main/java/com/example/bv/onixtest/MainActivity.java
+++ app/src/main/java/com/example/bv/onixtest/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.bv.onixtest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
@@ -18,39 +20,40 @@ import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import io.fabric.sdk.android.Fabric;
 
 
-
-/**
- * Created by bv on 02.07.16.
- */
 public class MainActivity extends Activity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "UFZbipXZHVUAlEXo9rF771vtv";
     private static final String TWITTER_SECRET = "jQvXj3Z9YFd1dl9HEimeCNDyXY8ClDNg7c9h2dR10S5sGGcDsG";
-    private Button loginButton;
-
+    private TwitterAuthConfig authConfig;
+   // loginButton;
+   TwitterLoginButton loginButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
-        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.main_layout);
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this,new Twitter(authConfig));
+
+
+        loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
-            public void onClick(View view) {
-                Twitter.logIn(MainActivity.this, new Callback<TwitterSession>() {
-                    @Override
-                    public void success(Result<TwitterSession> result) {
+            public void success(Result<TwitterSession> result) {
+                TwitterSession session =result.data;
+                Toast.makeText(MainActivity.this,session.getUserName(),Toast.LENGTH_LONG).show();
+            }
 
-                    }
+            @Override
+            public void failure(TwitterException exception) {
 
-                    @Override
-                    public void failure(TwitterException exception) {
-
-                    }
-                });
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      //  super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode,resultCode,data);
+    }
 }
